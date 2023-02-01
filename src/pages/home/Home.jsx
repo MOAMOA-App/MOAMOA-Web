ef1799933677be855cb274057205919c2714cb19
lgrin-byte <lgrin02@naver.com>
2023-02-01 21:46:58 +0900

    Feat : 레이아웃 컴포넌트 생성

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Header from '../../components/Header'
-import style from './home.module.css'
+import React from "react";
+import Header from "../../components/header/Header";
+import style from "./home.module.css";
 
 export default function Home() {
-  return (
-    <>
-      <div className={style.cont}>Home</div>
-    </>
-  )
+    return (
+        <>
+            <div className={style.cont}>Home</div>
+        </>
+    );
 }
