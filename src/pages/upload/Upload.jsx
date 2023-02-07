17a1274b4676b7d87789e69efd65ebde3130315d
lgrin-byte <lgrin02@naver.com>
2023-02-07 13:21:18 +0900

    Feat : Upload 페이지 마크업

--- src/pages/upload/Upload.jsx
+++ src/pages/upload/Upload.jsx
@@ -3,22 +3,27 @@ import style from './upload.module.css'
 export default function Upload() {
     return (
         <div className={style.wrap}>
-            <button>업로드</button>
+            <button className={style.btn_upload}>업로드</button>
             <img src="" alt="" />
-            <span>상품명</span>
+            <span className={style.span_upload} >상품명</span>
             <input type="text" name="" id="" />
-            <span>카테고리</span>
+            <span className={style.span_upload}>카테고리</span>
             <select name="" id=""></select>
-            <span>상품명</span>
+            <span className={style.span_upload}>상품 설명</span>
             <textarea name="" id="" cols="30" rows="10"></textarea>
-            <span>상품가격</span>
-            <input type="text" name="" id="" />
+            <span className={style.span_upload}>상품가격</span>
+        <div>
+            <input type="number" name="" id="" />
             <input type="checkbox" name="" id="" />
-            <span>모집인원</span>
-            <input type="text" name="" id="" />
-            <span>주소입력</span>
-            <input type="text" name="" id="" />
-            <button>이름</button>
+        </div>
+            <span className={style.span_upload}>모집인원</span>
+            <input className={style.inp_people} type="number" name="" id="" />
+            <span className={style.span_upload}>주소입력</span>
+            <div>
+
+            <input className={style.address} type="text" name="" id="" />
+            <button className={style.btn_search}>찾기</button>
+            </div>
             <input type="text" name="" id="" />
             <div>지도</div>
         </div>
