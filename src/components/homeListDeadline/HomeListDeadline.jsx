7e7e5f7741d0ffa0b63321f7258bff39768cca36
tasty13 <bodri0625@gmail.com>
2023-02-07 19:38:14 +0900

    Feat : 홈 화면 구현
    - 카테고리 추가 (img public에 추가)
    -

---
+++ src/components/homeListDeadline/HomeListDeadline.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from "./HomeListDeadline.module.css";
+
+export default function homeListDeadLine() {
+  return (
+    <>
+      <div className={style.homeWebDLCont}>
+          <div className={style.homeWebDLInfo}>
+              <h2>마감임박</h2>
+              <button>전체보기</button>
+          </div>
+          <div className={style.homeWebDLCont1}>
+                
+          </div>
+      </div>
+    </>
+  )
+}
---
+++ src/components/homeListLatest/HomeListLatest.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from "./HomeListLatest.module.css";
+
+export default function homeListLatest() {
+  return (
+    <>
+      <div className={style.homeWebLatestCont}>
+          <div className={style.homeWebLatestInfo}>
+              <h2>신규 공동구매</h2>
+              <button>전체보기</button>
+          </div>
+          <div className={style.homeWebLatestCont1}>
+                
+          </div>
+      </div>
+    </>
+  )
+}
