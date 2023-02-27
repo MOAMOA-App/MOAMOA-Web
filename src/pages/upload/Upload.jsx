77aeffd336f4df29866ce02d8a7a14cabd6f96b2
lgrin-byte <lgrin02@naver.com>
2023-02-27 19:50:38 +0900

    Feat : Upload 페이지 보완

--- src/pages/upload/Upload.jsx
+++ src/pages/upload/Upload.jsx
@@ -1,30 +1,46 @@
 import React from 'react'
 import style from './upload.module.css'
+
 export default function Upload() {
     return (
         <div className={style.wrap}>
             <button className={style.btn_upload}>업로드</button>
-            <img src="" alt="" />
+            <div className={style.wrap_img}>
+                <div className={style.cont_img}>
+                    <img src="" alt="" />
+                </div>
+                <div className={style.cont_img}>
+                    <img src="" alt="" />
+                </div>
+                <div className={style.cont_img}>
+                    <img src="" alt="" />
+                </div>
+            </div>
             <span className={style.span_upload} >상품명</span>
-            <input type="text" name="" id="" />
+            <input  className={style.input_upload}  type="text" name="" id="" />
             <span className={style.span_upload}>카테고리</span>
-            <select name="" id=""></select>
+            <select className={style.select_upload} name="" id=""></select>
             <span className={style.span_upload}>상품 설명</span>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <textarea  className={style.input_upload} name="" id="" cols="30" rows="10"></textarea>
             <span className={style.span_upload}>상품가격</span>
         <div>
-            <input type="number" name="" id="" />
-            <input type="checkbox" name="" id="" />
+            <input  className={style.input_upload} type="number" name="" id="" />
+            <span className={style.won_upload}>원</span>            
+            <input type="checkbox" name="" id="price" />
+            <label htmlFor="price">가격제한없음</label>
         </div>
             <span className={style.span_upload}>모집인원</span>
-            <input className={style.inp_people} type="number" name="" id="" />
+            <div>
+                <input className={style.inp_people} type="number" name="" id="" />
+                <span>명</span>
+            </div>
             <span className={style.span_upload}>주소입력</span>
             <div>
 
             <input className={style.address} type="text" name="" id="" />
             <button className={style.btn_search}>찾기</button>
             </div>
-            <input type="text" name="" id="" />
+            <input className={style.address}  type="text" name="" id="" />
             <div>지도</div>
         </div>
     )
