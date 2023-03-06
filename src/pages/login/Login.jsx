b3b2f45975f23b7b90607872759a72d792f4ae5a
tasty13 <bodri0625@gmail.com>
2023-03-06 18:45:09 +0900

    Test : 로그인 반응형화면

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,73 +1,33 @@
 import React from 'react'
 import style from "./login.module.css";
 import { useMediaQuery } from 'react-responsive'
+import LoginCont from '../../components/login/LoginCont.jsx'
 
-import googleIcon from '../../assets/images/loginsnsimage/googleIcon.png'
-import kakaoIcon from '../../assets/images/loginsnsimage/kakaoIcon.png'
-import naverIcon from '../../assets/images/loginsnsimage/naverIcon.png'
-
-const Tablet = ({ children }) => {
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 })
-  return isTablet ? children : null
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 })
+  return isDesktop ? children : null
 }
 const Mobile = ({ children }) => {
-  const isMobile = useMediaQuery({ maxWidth: 767 })
+  const isMobile = useMediaQuery({ maxWidth: 991 })
   return isMobile ? children : null
-}
-const Default = ({ children }) => {
-  const isNotMobile = useMediaQuery({ minWidth: 768 })
-  return isNotMobile ? children : null
 }
 
 export default function Login() {
   return (
-    <div className={style.login}>
-      <div className={style.loginCont1}>
-        <div className={style.loginCont2}>
-          <div className={style.loginEmailCont}>
-            <label for="loginInputEmail">이메일</label>
-            <input id="loginInputEmail" type="text"></input>
-
-            <label for="loginInputPw">비밀번호</label>
-            <input id="loginInputPw" type="password"></input>
-
-            <button className={style.loginEmailBtn}>로그인</button>
-          </div>
-
-          <div className={style.loginSnsCont}>
-            <button className={style.loginSnsKakao}>
-              <img className={style.loginSnsKakaoImg} src={kakaoIcon} alt="" />
-              카카오톡 계정으로 로그인
-              </button>
-            <button className={style.loginSnsGoogle}>
-              <img className={style.loginSnsGoogleImg} src={googleIcon} alt="" />
-              구글 계정으로 로그인
-              </button>
-            <button className={style.loginSnsNaver}>
-              <img className={style.loginSnsNaverImg} src={naverIcon} alt="" />
-              네이버 계정으로 로그인
-              </button>
-          </div>
-          
-          <div className={style.loginEtc}>
-
-            <a className='findIdPw' href="">
-              <span>아이디/비밀번호 찾기</span>
-            </a>
-
-            <span> | </span>
-
-            <a className='joinMOAMOA' href="/signup">
-              <span>회원가입</span>
-            </a>
+    <>
+      <Desktop>
+        <div className={style.login}>
+          <LoginCont/>
+          <div className={style.loginImageCont}>
+            <img className={style.loginImage} src="./icon.png" alt="머리 위에 귤이 있는 고양이"></img>
           </div>
         </div>
-      </div>
-
-      
-      <div className={style.loginImageCont}>
-        <img className={style.loginImage} src="./icon.png" alt="머리 위에 귤이 있는 고양이"></img>
-      </div>
-    </div>
+      </Desktop>
+      <Mobile>
+        <div className={style.login}>
+          <LoginCont/>
+        </div>
+      </Mobile>
+    </>
   )
 }
