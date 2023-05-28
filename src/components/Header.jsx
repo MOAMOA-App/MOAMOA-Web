4c1bac7fdb8e79c171674d6574c9a9160f2cb531
lgrin-byte <lgrin02@naver.com>
2023-05-28 23:56:55 +0900

    Feat : Header 컴포넌트 생성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import  logo  from '../assets/images/logo.png';
 import  title  from '../assets/images/title.png';
+import  chat  from '../assets/images/chat.svg';
+import  alarm  from '../assets/images/alarm.svg';
 import styled from 'styled-components';
 
 export default function Header() {
@@ -11,17 +13,15 @@ export default function Header() {
                 <img src={logo} alt="" />
                 <img src={title} alt="" />
             </div>
-            <div>
+            <Wrap>
                 <Write to="/write">공동구매 모집</Write>
                 <Link to="/chat">채팅</Link>
                 <Link to="/alarm">알림</Link>
                 <Link to="/profile">프로필</Link>
-            </div>
+            </Wrap>
         </Div>
     )
 }
-
-
 
 export const Div = styled.div`
     display: flex;
@@ -31,10 +31,28 @@ export const Div = styled.div`
 
 `;
 
+export const Wrap = styled.div`
+    display: flex;
+`;
+
 export const Write = styled(Link)`
-	box-sizing: border-box;
-	display: block;
-	padding: 4px 8px;
-	margin: 0 auto;
-	text-align: center;
+    width: 132px;
+    height: 40px;
+    display: flex;
+    background: #F79331;
+    border-radius: 3px;
+    align-items: center;
+    text-align: center;
+    font-size: 14px;
+    color: #FFFFFF;
+    font-weight: 700;
+`;
+
+export const Chat = styled(Link)`
+`;
+
+export const Alarm = styled(Link)`
+`;
+
+export const Profile = styled(Link)`
 `;
