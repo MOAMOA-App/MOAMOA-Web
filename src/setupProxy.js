103fc88a11900505fd79edb5c9ef9142bd390193
lgrin-byte <lgrin02@naver.com>
2023-10-10 23:54:02 +0900

    Chore: 프록시 설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,14 @@
-//setupProxy.js
-
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware("/3.36.250.168:80", {
-            target: "http://3.36.250.168:80",
+        createProxyMiddleware("/api", {
+            // "/api" 경로에 대한 요청을 프록시합니다.
+            target: "http://3.36.250.168:80", // 목표 서버 주소입니다.
             pathRewrite: {
-                "^/3.36.250.168:80": "",
+                "^/api": "", // 프록시 경로를 제거합니다.
             },
-            changeOrigin: true,
+            changeOrigin: true, // 호스트 헤더를 변경합니다.
         })
     );
 };
