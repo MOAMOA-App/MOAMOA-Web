94540875ff87a0b0aff7aee310b470817b5493c5
lgrin-byte <lgrin02@naver.com>
2023-10-20 00:00:03 +0900

    Chore: url추가

--- src/libary/reactQueryProvider.tsx
+++ src/libary/reactQueryProvider.tsx
@@ -1,9 +1,9 @@
-import React,{ PropsWithChildren, useState } from "react";
+import React, { PropsWithChildren, useState } from "react";
 import axios from "axios";
 import {
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
+    QueryCache,
+    QueryClient,
+    QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getAccessToken, removeAccessToken } from "../utils/localStorage";
@@ -11,70 +11,70 @@ import { getAccessToken, removeAccessToken } from "../utils/localStorage";
 const accessToken = getAccessToken();
 
 export const apiClient = axios.create({
-  withCredentials: true,
-  headers: {
-    ...(accessToken && {
-      Authorization: `Bearer ${accessToken.replace(/^"(.*)"$/, "$1")}`,
-    }),
-  },
+    withCredentials: true,
+    baseURL: "http://localhost:3000/",
+    headers: {
+        ...(accessToken && {
+            Authorization: `Bearer ${accessToken.replace(/^"(.*)"$/, "$1")}`,
+        }),
+    },
 });
 
 apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      // Authorization 헤더 삭제
-      delete apiClient.defaults.headers.common.Authorization;
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            // Authorization 헤더 삭제
+            delete apiClient.defaults.headers.common.Authorization;
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
 
 type ErrorResponse = {
-  response: {
-    data: {
-      code: number;
-      message: string;
+    response: {
+        data: {
+            code: number;
+            message: string;
+        };
+        status: number;
     };
-    status: number;
-  };
 };
 
 type DataResponse = {
-  code: number;
-  message: string;
+    code: number;
+    message: string;
 };
 
 export default function ReactQueryProvider({ children }: PropsWithChildren) {
-  const queryCache = new QueryCache({
-    onSettled: (data, error) => {
-      if (
-        (error as ErrorResponse)?.response?.data?.code === 401 ||
-        (error as ErrorResponse)?.response?.data?.code === 400 ||
-        (error as ErrorResponse).response.status === 401 ||
-        (error as ErrorResponse).response.status === 400 ||
-        (data as DataResponse)?.code === 401 ||
-        (data as DataResponse)?.code === 400
-      ) {
-        removeAccessToken();
-        delete apiClient.defaults.headers.common.Authorization;
-      }
-    },
-    onError: (error) => {
-    },
-  });
+    const queryCache = new QueryCache({
+        onSettled: (data, error) => {
+            if (
+                (error as ErrorResponse)?.response?.data?.code === 401 ||
+                (error as ErrorResponse)?.response?.data?.code === 400 ||
+                (error as ErrorResponse).response.status === 401 ||
+                (error as ErrorResponse).response.status === 400 ||
+                (data as DataResponse)?.code === 401 ||
+                (data as DataResponse)?.code === 400
+            ) {
+                removeAccessToken();
+                delete apiClient.defaults.headers.common.Authorization;
+            }
+        },
+        onError: (error) => {},
+    });
 
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        queryCache,
-      })
-  );
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                queryCache,
+            })
+    );
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 }
