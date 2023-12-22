a712f7b3b9b62489d10e1a1a2bfc50fc6664762d
lgrin-byte <lgrin02@naver.com>
2023-12-22 22:21:41 +0900

    docs:alter utils

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         createProxyMiddleware("/api", {
             // "/api" 경로에 대한 요청을 프록시합니다.
-            target: "http://3.36.250.168:80", // 목표 서버 주소입니다.
+            target: "https://moamoa.info", // 목표 서버 주소입니다.
             pathRewrite: {
                 "^/api": "", // 프록시 경로를 제거합니다.
             },
--- src/utils/getTime.ts
+++ src/utils/getTime.ts
@@ -1,23 +1,35 @@
-export const getTime = (time: Date) => {
-  const today = new Date();
+export const getTime = (dateString: string) => {
+    const time = new Date(dateString);
+    const today = new Date();
 
-  const betweenTime = Math.floor(
-    (today.getTime() - time.getTime()) / 1000 / 60,
-  );
-  if (betweenTime < 1) return "방금 전";
-  if (betweenTime < 60) {
-    return `${betweenTime}분 전`;
-  }
+    const betweenTime = Math.floor(
+        (today.getTime() - time.getTime()) / 1000 / 60
+    );
+    if (betweenTime < 1) return "방금 전";
+    if (betweenTime < 60) {
+        return `${betweenTime}분 전`;
+    }
 
-  const betweenTimeHour = Math.floor(betweenTime / 60);
-  if (betweenTimeHour < 24) {
-    return `${betweenTimeHour}시간 전`;
-  }
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+        return `${betweenTimeHour}시간 전`;
+    }
 
-  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
-  if (betweenTimeDay < 365) {
-    return `${betweenTimeDay}일 전`;
-  }
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 365) {
+        return `${betweenTimeDay}일 전`;
+    }
 
-  return `${Math.floor(betweenTimeDay / 365)}년 전`;
+    return `${Math.floor(betweenTimeDay / 365)}년 전`;
+};
+
+export const getDate = (dateString: string) => {
+    const date = new Date(dateString);
+
+    // 년, 월, 일을 추출하고 포매팅
+    const year = date.getFullYear().toString().substr(-2); // 마지막 두 자리만 추출
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // 월은 0부터 시작하므로 +1 필요
+    const day = date.getDate().toString().padStart(2, "0");
+
+    return `${year}.${month}.${day}`;
 };
